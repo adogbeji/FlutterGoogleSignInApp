5db1ae8b7de4746b056b3b3763891af843c4d577
adogbeji <baka_1993@yahoo.co.uk>
2024-07-16 20:14:28 +0100

    Add name, email & password input fields to register screen

--- lib/views/screens/auth/register_screen.dart
+++ lib/views/screens/auth/register_screen.dart
@@ -5,9 +5,55 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Register Screen'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Register',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
+                ),
+              ),
+        
+              // NAME INPUT FIELD
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  hintText: 'Enter Name',
+                  prefixIcon: Icon(Icons.person, color: Colors.pinkAccent,),
+                ),
+              ),
+        
+              const SizedBox(height: 12,),
+        
+              // EMAIL INPUT FIELD
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter Email',
+                  prefixIcon: Icon(Icons.email, color: Colors.pinkAccent,),
+                ),
+              ),
+        
+              const SizedBox(height: 12,),
+        
+              // PASSWORD INPUT FIELD
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter Password',
+                  prefixIcon: Icon(Icons.lock, color: Colors.pinkAccent,),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
